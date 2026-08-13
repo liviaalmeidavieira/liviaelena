8e83069e96484df690cc59fda881fd79021a1f93
liviaalmeidavieira <livia.almeida.vieira@escola.pr.gov.br>
2026-08-13 10:59:50 -0300

    arrumamos o codigo dos botões.

---
+++ script.js
@@ -0,0 +1,15 @@
+// Botões
+const botoes = document.querySelectorAll('button');
+
+// Para cada botão podemos curtir
+botoes.forEach(botao => {
+    let curtiu = false;
+
+    botao.addEventListener("click", curtir);
+
+    function curtir(){
+        let num = botao.querySelector('span');
+
+        num.textContent++;
+    }
+});
